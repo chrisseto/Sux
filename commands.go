2b8ceb36ac47a65425dc542a72f2e4e4437ce83c
Chris Seto <chriskseto@gmail.com>
2015-07-15 02:20:30 -0400

    Printing is now working again

--- commands.go
+++ commands.go
@@ -5,6 +5,8 @@ import (
 	"github.com/nsf/termbox-go"
 	"os"
 	"strings"
+  "fmt"
+	// "strconv"
 )
 
 var (
@@ -19,7 +21,7 @@ func RunPanes() error {
 	quitChan = make(chan bool)
 	selectChan = make(chan *Pane)
 	width, height := termbox.Size()
-	uwidth, uheight := uint16(width), uint16(height)
+	uwidth, uheight := uint16(width), uint16(height-1)
 	cmds := strings.Split(strings.Join(flag.Args(), " "), ",")
 
 	RunningPanes = make([]*Pane, len(cmds))
@@ -41,10 +43,20 @@ func RunPanes() error {
 
 func OutputLoop() {
 	selected := <-selectChan
+  f, _ := os.Create("log.log")
+  defer f.Close()
+	WriteStatusBar(selected.Prog)
+	termbox.Flush()
 	for {
 		select {
-		case buf := <-selected.Output:
-			os.Stdout.Write(buf)
+		case cells := <-selected.CellOutput:
+      fmt.Fprintf(f, "%v", cells)
+			for _, cell := range cells {
+        fmt.Fprintf(f, "[%v, %v, %#U]\n", cell.x, cell.y, cell.Ch)
+				termbox.SetCell(cell.x, cell.y, cell.Ch, cell.Fg, cell.Bg)
+			}
+			WriteStatusBar(selected.Prog)
+			termbox.Flush()
 		case selected = <-selectChan:
 		case <-quitChan:
 			return
@@ -55,7 +67,14 @@ func OutputLoop() {
 func setPane(index int) {
 	SelectedPane = RunningPanes[index]
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	termbox.Sync()
+	for y, line := range SelectedPane.Cells() {
+		for x, cell := range line {
+			termbox.SetCell(x, y, cell.Ch, cell.Fg, cell.Bg)
+		}
+	}
+	WriteStatusBar(SelectedPane.Prog)
+	// termbox.Sync()
+	termbox.Flush()
 	selectChan <- SelectedPane
 }
 
@@ -80,4 +99,18 @@ func EndPanes() {
 	for _, cmd := range RunningPanes {
 		cmd.Close()
 	}
+}
+
+func WriteStatusBar(prog string) {
+	width, height := termbox.Size()
+	i := 0
+	for _, char := range prog {
+		i++
+		termbox.SetCell(i, height-1, char, termbox.ColorBlack, termbox.ColorGreen)
+	}
+	for ; i < width; i++ {
+		termbox.SetCell(i, height-1, ' ', termbox.ColorBlack, termbox.ColorGreen)
+	}
+	// termbox.HideCursor()
+	// termbox.Flush()
 }
--- pane.go
+++ pane.go
@@ -3,32 +3,41 @@ package main
 import (
 	"bufio"
 	"github.com/chrisseto/pty"
+	"github.com/nsf/termbox-go"
 	"io"
 	"os"
-	// "github.com/nsf/termbox-go"
 	"os/exec"
+  "fmt"
 )
+
+type Cell struct {
+	termbox.Cell
+	x, y int
+}
 
 type Pane struct {
 	*exec.Cmd
 
 	cx, cy        int
+	sx, sy        int
 	width, height uint16
 
-	prog string
+	Prog string
 	args []string
 
-	Pty    *os.File
-	output io.Reader
-	// cells []termbox.Cell
-	Output chan []byte
+	Pty        *os.File
+	output     io.Reader
+	cells      [][]termbox.Cell
+	Output     chan []byte
+	CellOutput chan []Cell
 }
 
 func CreatePane(width, height uint16, prog string, args ...string) *Pane {
 	return &Pane{
 		Cmd: exec.Command(prog, args...),
 		cx:  0, cy: 0,
-		prog: prog, args: args,
+		sx:  0, sy: 0,
+		Prog: prog, args: args,
 		width: width, height: height,
 		Pty: nil, output: nil, Output: nil,
 	}
@@ -44,7 +53,10 @@ func (p *Pane) Start() error {
 	}
 	p.Pty = pterm
 	p.Output = make(chan []byte, 32)
+	p.CellOutput = make(chan []Cell, 32)
 	p.output = bufio.NewReader(p.Pty)
+	p.cells = make([][]termbox.Cell, 1, p.height)
+	p.cells[0] = make([]termbox.Cell, 0, p.width)
 	go p.outputPipe()
 	return nil
 }
@@ -53,14 +65,51 @@ func (p *Pane) Close() error {
 	return p.Process.Kill()
 }
 
+func (p *Pane) Cells() [][]termbox.Cell {
+	return p.cells
+}
+
+func (p *Pane) Width() uint16 {
+	return p.width
+}
+
+func (p *Pane) Height() uint16 {
+	return p.height
+}
+
 func (p *Pane) outputPipe() {
 	buf := make([]byte, 32*1024)
+  f, _ := os.Create("outpipe.log")
+  defer f.Close()
 	for {
 		nr, err := p.output.Read(buf)
 		if nr > 0 {
-			c := make([]byte, nr)
-			copy(c, buf[:nr])
-			p.Output <- c
+			b := make([]Cell, 0, nr)
+			row := &p.cells[p.sy]
+
+			for _, char := range buf[:nr] {
+				switch char {
+        case 0xA:
+          p.sy++
+					p.cells = append(p.cells, nil)
+					*row = p.cells[p.sy]
+				case 0xD:
+					p.sx = 0
+				case 0x8:
+          p.sx--
+					*row = (*row)[:p.sx]
+          c := Cell{termbox.Cell{' ', 0x0, 0x0}, p.sx, p.sy}
+          b = append(b, c)
+				default:
+          c := Cell{termbox.Cell{rune(char), 0x0, 0x0}, p.sx, p.sy}
+					*row = append(*row, c.Cell)
+          b = append(b, c)
+					p.sx++
+				}
+			}
+      fmt.Fprintf(f, "nr: %v\nbuf: %v\n%+v\n", nr, buf[:nr], b)
+
+			p.CellOutput <- b
 		}
 		if err != nil {
 			if err == io.EOF {
