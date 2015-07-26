58bc0c7a6f026db083914eb1a8102743b2dec68d
Chris Seto <chriskseto@gmail.com>
2015-07-26 15:28:53 -0400

    CursorPosition defualts to 0,0

--- pane.go
+++ pane.go
@@ -166,7 +166,11 @@ func (p *Pane) handleEscapeCode(c *pansi.AnsiEscapeCode) {
 	case pansi.SetGraphicMode:
 		p.SetGraphicMode(c.Values)
 	case pansi.CursorPosition:
-		p.cx, p.cy = c.Values[1], c.Values[2]
+		if len(c.Values) == 0 {
+			p.cx, p.cy = 0, 0
+		} else {
+			p.cx, p.cy = c.Values[1], c.Values[2]
+		}
 	case pansi.CursorUp:
 		p.cy--
 	case pansi.CursorDown:
