134f5ffe34be65b0668823dbadb4ab2785b44f2c
Chris Seto <chriskseto@gmail.com>
2016-04-22 21:16:18 -0400

    More ring buffer updates

--- pane/ring.go
+++ pane/ring.go
@@ -6,12 +6,14 @@ import (
 )
 
 type RingBuffer struct {
-	buffer [][]termbox.Cell
+	len    int
 	index  int
+	buffer [][]termbox.Cell
 }
 
 func NewRingBuffer(initial [][]termbox.Cell) RingBuffer {
 	r := RingBuffer{
+		len:    len(initial),
 		index:  0,
 		buffer: initial,
 	}
@@ -19,22 +21,33 @@ func NewRingBuffer(initial [][]termbox.Cell) RingBuffer {
 	return r
 }
 
+func (r *RingBuffer) Length() int {
+	return r.len
+}
+
+func (r *RingBuffer) Clear() {
+	r.index++
+	r.len = 0
+}
+
 func (r *RingBuffer) Append(data []termbox.Cell) {
-	log.Printf("Appending at index %d with length %d", r.index, len(r.buffer))
 	if cap(r.buffer) != len(r.buffer) {
 		r.buffer = append(r.buffer, data)
-		return
+	} else {
+		r.buffer[r.offset(r.len)] = data
 	}
-	r.buffer[r.index] = data
 
-	r.index++
-	if r.index >= len(r.buffer) {
-		r.index = 0
+	if r.len < cap(r.buffer) {
+		r.len++
+	} else {
+		r.index++
+		if r.index >= cap(r.buffer) {
+			r.index = 0
+		}
 	}
 }
 
 func (r *RingBuffer) Get(i int) []termbox.Cell {
-	log.Printf("Getting index %d, internal index %d, resolved to %d", i, r.index, r.offset(i))
 	return r.buffer[r.offset(i)]
 }
 
@@ -43,29 +56,23 @@ func (r *RingBuffer) Set(i int, data []termbox.Cell) {
 }
 
 func (r *RingBuffer) offset(i int) int {
-	if cap(r.buffer) != len(r.buffer) {
-		return i
-	}
-	if r.index+i >= len(r.buffer) {
-		return (r.index + i) - len(r.buffer)
-	}
-	return r.index + i
+	return (r.index + i) % cap(r.buffer)
 }
 
 func (r *RingBuffer) Range(begin, length int) [][]termbox.Cell {
 	start := r.offset(begin)
-	if length > len(r.buffer) {
-		length = len(r.buffer)
+	if length > r.Length() {
+		length = r.Length()
 	}
+
 	//Golang slices are EXCLUSIVE
 	//IE [1...10][0:9] -> [1,2,3,4,5,6,7,8]
 	end := r.offset(begin + length - 1)
+	log.Printf("Range(%d, %d) -> [%d : %d]", begin, length, start, end)
 
 	if end < start {
-		log.Printf("Range request of %d-%d resolved to %d-%d + %d-%d", begin, begin+length, start, len(r.buffer)-1, 0, end)
 		return append(r.buffer[start:len(r.buffer)], r.buffer[0:end+1]...)
 	}
 
-	log.Printf("Range request of %d-%d resolved to %d-%d", begin, begin+length, start, end)
 	return r.buffer[start : end+1]
 }
--- pane/ring_test.go
+++ pane/ring_test.go
@@ -8,7 +8,10 @@ import (
 func TestAppend(t *testing.T) {
 	ring := NewRingBuffer(make([][]termbox.Cell, 0, 10))
 
-	for i := 0; i < 9; i++ {
+	for i := 0; i < 10; i++ {
+		if ring.Length() != i {
+			t.Error("Expected ring.Length() to be", i, ". Got", ring.Length())
+		}
 		ring.Append(make([]termbox.Cell, 1))
 	}
 
@@ -16,9 +19,15 @@ func TestAppend(t *testing.T) {
 		ring.Append([]termbox.Cell{
 			termbox.Cell{rune(i), 0x0, 0x0},
 		})
+		t.Logf("Offset for 9 is %d", ring.offset(9))
+
 		if ring.Get(9)[0].Ch != rune(i) {
 			t.Error("Expected ring.Get(9) to be", rune(i), ". Got", ring.Get(9)[0].Ch)
 		}
+	}
+
+	if ring.Length() != 10 {
+		t.Error("Expected ring.Length() to be 10 . Got", ring.Length())
 	}
 }
 
@@ -60,6 +69,71 @@ func TestRange(t *testing.T) {
 
 		ring.Append([]termbox.Cell{
 			termbox.Cell{rune(i + 10), 0x0, 0x0},
+		})
+	}
+}
+
+func TestClear(t *testing.T) {
+	ring := NewRingBuffer(make([][]termbox.Cell, 0, 10))
+
+	for i := 0; i < 10; i++ {
+		ring.Append([]termbox.Cell{
+			termbox.Cell{rune(i), 0x0, 0x0},
+		})
+	}
+
+	for i := 0; i < 100; i++ {
+		ring.Clear()
+		if ring.Length() != 0 {
+			t.Error("Expected ring.Length() to be 1. Got", ring.Length())
+		}
+		ring.Append([]termbox.Cell{
+			termbox.Cell{rune(i), 0x0, 0x0},
+		})
+
+		for j := 1; j < 5; j++ {
+			if ring.Length() != j {
+				t.Errorf("Expected ring.Length() to be %d. Got %d", j, ring.Length())
+			}
+			ring.Append([]termbox.Cell{
+				termbox.Cell{rune(0), 0x0, 0x0},
+			})
+		}
+
+		if ring.Get(0)[0].Ch != rune(i) {
+			t.Error("Expected ring.Get(0) to be", rune(i), ". Got", ring.Get(0)[0].Ch)
+		}
+	}
+}
+
+func TestClearPreserves(t *testing.T) {
+	ring := NewRingBuffer(make([][]termbox.Cell, 0, 10))
+
+	for i := 0; i < 10; i++ {
+		ring.Append([]termbox.Cell{
+			termbox.Cell{rune(i), 0x0, 0x0},
+		})
+	}
+
+	ring.Clear()
+
+	if ring.Length() != 0 {
+		t.Errorf("Expected ring.Length() to be 0. Got %d", ring.Length())
+	}
+
+	for i := 0; i < 10; i++ {
+		if ring.buffer[i][0].Ch != rune(i) {
+			t.Errorf("Expected ring.buffer[%d][0].Ch to be %s. Got %s", i, string(i), string(ring.buffer[i][0].Ch))
+		}
+	}
+}
+
+func BenchmarkAppending(b *testing.B) {
+	ring := NewRingBuffer(make([][]termbox.Cell, 0, 10))
+
+	for i := 0; i < b.N; i++ {
+		ring.Append([]termbox.Cell{
+			termbox.Cell{rune(i), 0x0, 0x0},
 		})
 	}
 }
