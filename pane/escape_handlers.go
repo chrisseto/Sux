3f00398f6201e8ded299a0a22c8f818cbe6b3c57
Chris Seto <chriskseto@gmail.com>
2016-04-23 01:50:35 -0400

    Reimplement color parsing

--- pane/escape_handlers.go
+++ pane/escape_handlers.go
@@ -10,6 +10,7 @@ type EscapeCodeHandler func(p *Pane, c *pansi.AnsiEscapeCode)
 
 var ESCAPE_HANDLERS = map[pansi.AnsiEscapeType]EscapeCodeHandler{
 	pansi.EraseDisplay:   (*Pane).Clear,
+	pansi.SetGraphicMode: (*Pane).SetGraphicMode,
 	pansi.CursorPosition: (*Pane).CursorPosition,
 }
 
@@ -26,6 +27,55 @@ func (p *Pane) defaultEscapeCodeHandler(c *pansi.AnsiEscapeCode) {
 }
 
 func (p *Pane) SetGraphicMode(c *pansi.AnsiEscapeCode) {
+
+	switch {
+	case len(c.Values) == 0:
+		fallthrough
+	case c.Values[0] == 0:
+		// Reset to default colors
+		p.fg, p.bg = 8, 1
+
+	case c.Values[0] == 1:
+		// Set text to bold
+		p.fg |= termbox.AttrBold
+
+	case c.Values[0] == 7:
+		// Inverse fg/bg
+		p.fg, p.bg = p.bg, p.fg
+
+	case 30 <= c.Values[0] && c.Values[0] <= 37:
+		// Set fg to Black/Red/Green/Yellow/Blue/Magenta/Cyan
+		p.fg = termbox.Attribute(c.Values[1] - 29) //-30 + 1 for termbox offset
+
+	case c.Values[0] == 38:
+		if c.Values[1] == 5 {
+			p.fg = termbox.Attribute(c.Values[2] + 1)
+		} else if c.Values[1] == 2 {
+			//TODO Parse RGB color code
+		} else {
+			panic("Malformed SGR Code")
+		}
+
+	case c.Values[0] == 39:
+		p.fg = termbox.ColorWhite
+
+	case 40 <= c.Values[0] && c.Values[0] <= 47:
+		// Set bg to Black/Red/Green/Yellow/Blue/Magenta/Cyan
+		p.bg = termbox.Attribute(c.Values[1] - 29) //-30 + 1 for termbox offset
+
+	case c.Values[0] == 48:
+		if c.Values[1] == 5 {
+			p.bg = termbox.Attribute(c.Values[2] + 1)
+		} else if c.Values[1] == 2 {
+			//TODO Parse RGB color code
+		} else {
+			panic("Malformed SGR Code")
+		}
+
+	case c.Values[0] == 49:
+		p.bg = termbox.ColorBlack
+	}
+
 }
 
 func (p *Pane) Clear(c *pansi.AnsiEscapeCode) {
