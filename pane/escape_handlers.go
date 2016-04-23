3427fe26f1f409ea9441111bc7c7b5416419bac2
Chris Seto <chriskseto@gmail.com>
2016-04-23 01:24:58 -0400

    Add additional escape handlers

--- pane/escape_handlers.go
+++ pane/escape_handlers.go
@@ -2,16 +2,20 @@ package pane
 
 import (
 	"github.com/chrisseto/sux/pansi"
+	"github.com/nsf/termbox-go"
 	"log"
 )
 
-type EscapeCodeHandler func(p *Pane)
+type EscapeCodeHandler func(p *Pane, c *pansi.AnsiEscapeCode)
 
-var ESCAPE_HANDLERS = map[pansi.AnsiEscapeType]EscapeCodeHandler{}
+var ESCAPE_HANDLERS = map[pansi.AnsiEscapeType]EscapeCodeHandler{
+	pansi.EraseDisplay:   (*Pane).Clear,
+	pansi.CursorPosition: (*Pane).CursorPosition,
+}
 
 func (p *Pane) handleEscapeCode(c *pansi.AnsiEscapeCode) {
 	if handler, ok := ESCAPE_HANDLERS[c.Type]; ok {
-		handler(p)
+		handler(p, c)
 	} else {
 		p.defaultEscapeCodeHandler(c)
 	}
@@ -21,5 +25,18 @@ func (p *Pane) defaultEscapeCodeHandler(c *pansi.AnsiEscapeCode) {
 	log.Printf("Go unhandled escape code %+v\n", c)
 }
 
-func (p *Pane) setGraphicMode(c *pansi.AnsiEscapeCode) {
+func (p *Pane) SetGraphicMode(c *pansi.AnsiEscapeCode) {
+}
+
+func (p *Pane) Clear(c *pansi.AnsiEscapeCode) {
+	p.buffer.Clear()
+	p.buffer.Append(make([]termbox.Cell, p.width))
+}
+
+func (p *Pane) CursorPosition(c *pansi.AnsiEscapeCode) {
+	if len(c.Values) == 0 {
+		p.cursor.Set(0, 0)
+	} else {
+		p.cursor.Set(c.Values[1]-1, c.Values[0]-1)
+	}
 }
